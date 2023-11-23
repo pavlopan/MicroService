e6415c8100825fc3fb8f9ec90543a92cf692f111
pavlop <pavlopan@gmail.com>
2023-11-23 20:39:16 +0200

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import static spark.Spark.*;
+
+public class Main {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+    }
+}
